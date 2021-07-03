efbe4ff5321e5fea0efba1933dc06a449e33993d
bartvriesema <bart@vriesema.org>
2021-07-03 09:49:28 +0200

    Initial commit

---
+++ src/main/java/org/vriesema/novi/rowingapp/RowingappApplication.java
@@ -0,0 +1,13 @@
+package org.vriesema.novi.rowingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RowingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vriesema/novi/rowingapp/RowingappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vriesema.novi.rowingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
