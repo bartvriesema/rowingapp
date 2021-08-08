bb6f725fa79bc3f563e50a985356bb498a9d4a25
bartvriesema <bart@vriesema.org>
2021-08-08 11:05:12 +0200

    Added ResultsRepository

---
+++ src/main/java/org/vriesema/novi/rowingapp/model/rowingclub/Results.java
@@ -0,0 +1,75 @@
+package org.vriesema.novi.rowingapp.model.rowingclub;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Results {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private LocalDate resultDate;
+
+    @Column
+    private String description;
+
+    @Column
+    private int distance;
+
+    @Column
+    private int timeInSeconds;
+
+    @ManyToOne
+    private Crew crew;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDate getResultDate() {
+        return resultDate;
+    }
+
+    public void setResultDate(LocalDate resultDate) {
+        this.resultDate = resultDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getTimeInSeconds() {
+        return timeInSeconds;
+    }
+
+    public void setTimeInSeconds(int timeInSeconds) {
+        this.timeInSeconds = timeInSeconds;
+    }
+
+    public Crew getCrew() {
+        return crew;
+    }
+
+    public void setCrew(Crew crew) {
+        this.crew = crew;
+    }
+}
